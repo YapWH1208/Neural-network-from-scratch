3b043b4a8ce3626d3af280bec0ef826bfe0df0a8
YapWH <107160166+YapWH1208@users.noreply.github.com>
2023-07-31 13:39:57 +0800

    1.1
    commit normalization function

--- WHNNT/preprocessing.py
+++ WHNNT/preprocessing.py
@@ -33,3 +33,47 @@ class Initializer():
         """
         limit = np.sqrt(2 / dim1 * (1 + alpha**2))
         return np.random.uniform(-limit, limit, (dim1, dim2))
+
+class Normalizer():
+    def __init__(self, x):
+        self.X = x
+    
+    def Min_Max_Normalization(self):
+        """
+        Perform Min-Max normalization on the input data.
+
+        Returns:
+        list: Normalized data with values scaled between 0 and 1.
+        """
+        if not self.X:
+            raise ValueError("Input data is empty.")
+
+        if isinstance(self.X, np.ndarray):
+            self.X = self.X.tolist()
+
+        min_value = min(self.X)
+        max_value = max(self.X)
+
+        normalized_data = [(value - min_value) / (max_value - min_value) for value in self.X]
+
+        return normalized_data
+
+    def z_score_standardization(self):
+        """
+        Perform Z-score standardization on the input data.
+
+        Returns:
+        list: Standardized data with a mean of 0 and standard deviation of 1.
+        """
+        if not self.X:
+            raise ValueError("Input data is empty.")
+
+        if isinstance(self.X, np.ndarray):
+            self.X = self.X.tolist()
+
+        mean_value = np.mean(self.X)
+        standard_deviation = np.std(self.X)
+
+        standardized_data = [(value - mean_value) / standard_deviation for value in self.X]
+
+        return standardized_data
